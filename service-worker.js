8bb337a61c8ee16071d8b1fd67fcc9b75df8a399
Jonatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2024-07-24 15:28:49 -0300

    atualização 12

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-// Evento de instalação do Service Worker
-self.addEventListener('install', event => {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('v1').then(cache => {
+        caches.open('v1').then(function (cache) {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/styles.css',  
+                '/css/styles.css',
+                '/js/app.js',
             ]);
         })
     );
 });
 
-// Evento de busca (fetch) do Service Worker
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
         })
     );
