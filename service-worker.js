24aec104e578111bd35aecd079810b4c8b371573
Jonatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2024-07-24 15:04:19 -0300

    atualização 07

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,25 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then( (cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/css/styles.css',
-          '/js/app.js',
-          '/imagens/icon-192x192.png',
-          '/imagens/icon-512x512.png'
-        ]);
+// Evento de instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+      caches.open('v1').then(cache => {
+          return cache.addAll([
+              '/',
+              '/index.html',
+              '/css/styles.css',
+              '/js/app.js',
+              '/imagens/icon-192x192.png',
+              '/imagens/icon-512x512.png'
+          ]);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  );
+});
+
+// Evento de busca (fetch) do Service Worker
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      caches.match(event.request).then(response => {
+          // Retorna o recurso do cache, ou faz a requisição de rede caso não esteja no cache
+          return response || fetch(event.request);
+      })
+  );
+});
