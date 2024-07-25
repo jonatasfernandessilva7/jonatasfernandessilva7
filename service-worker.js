1ddda45b3effe34312ad7bc81031992cfd3ca1db
Jonatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2024-07-25 08:40:14 -0300

    mais uma tentativa

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,19 @@
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('v1').then(function (cache) {
+        caches.open('v1').then(function(cache) {
             return cache.addAll([
                 '/',
+                '/jonatasfernandessilva7/index.html',
+                '/jonatasfernandessilva7/styles.css', // Exemplo de outros recursos
+                '/jonatasfernandessilva7/script.js'  // Exemplo de outros recursos
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
+        caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
     );
