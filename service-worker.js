45e290d991bae5802224c229a15e3f0768fe6da0
Jonatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2024-07-25 09:26:00 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,8 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll([
-                '/',
+                '/index.html',
+                '/styles.css'
             ]).catch(function(error) {
                 console.error('Failed to cache resources:', error);
             });
