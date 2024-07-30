82ea4d8dd50db821181842b01d12eda542252097
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2024-07-30 14:04:50 -0300

    finalmente 5

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const buttonIRProjeto1 = document.getElementById('Ir_projeto1');
 const buttonIRProjeto2 = document.getElementById('Ir_projeto2');
-const buttonIRprojetos = document.getElementById('projetos');
-const buttonHabilidades = document.getElementById('habilidades');
 
 buttonIRProjeto1.addEventListener('click', () => {
     console.log('oi');
@@ -11,12 +9,4 @@ buttonIRProjeto1.addEventListener('click', () => {
 buttonIRProjeto2.addEventListener('click', () => {
     console.log('oi');
     window.location.href="https://github.com/jonatasfernandessilva7/tasks-FreeRTOS-Linux.git"
-})
-
-buttonIRprojetos.addEventListener('click', () => {
-    window.location.href="../html/projetos.html"
-})
-
-buttonHabilidades.addEventListener('click', () => {
-    window.location.href="../html/habilidades.html"
 })
