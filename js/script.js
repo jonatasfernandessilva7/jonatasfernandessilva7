dbb2acddd63ec137e6a61cb3695a92c23cea9b04
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2024-07-30 14:32:13 -0300

    finalmente 6

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const buttonIRProjeto1 = document.getElementById('Ir_projeto1');
-const buttonIRProjeto2 = document.getElementById('Ir_projeto2');
+let buttonIRProjeto1 = document.getElementById('Ir_projeto1');
+let buttonIRProjeto2 = document.getElementById('Ir_projeto2');
 
 buttonIRProjeto1.addEventListener('click', () => {
     console.log('oi');
-    window.location.href="https://github.com/jonatasfernandessilva7/backend-schoolLab.git"
+    window.location.href= "https://github.com/jonatasfernandessilva7/backend-schoolLab.git"
 })
 
 buttonIRProjeto2.addEventListener('click', () => {
     console.log('oi');
-    window.location.href="https://github.com/jonatasfernandessilva7/tasks-FreeRTOS-Linux.git"
+    window.location.href= "https://github.com/jonatasfernandessilva7/tasks-FreeRTOS-Linux.git"
 })
