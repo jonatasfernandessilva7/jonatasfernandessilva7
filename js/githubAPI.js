16f2fdb40425e72f4e18288a5515afa412366177
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2025-06-19 08:39:57 -0300

    refs: projetos vindo da api do github

---
+++ js/githubAPI.js
@@ -0,0 +1,36 @@
+const repositories = document.querySelector('.parallax');
+
+function getAPIGithubRepositories() {
+    fetch('https://api.github.com/users/jonatasfernandessilva7/repositories')
+        .then(async res => {
+            if(!res.ok) {
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+
+            console.log(data);
+
+            data.map((item) => {
+                let project = document.createElement('div');
+
+                project.innerHTML = `
+                    <section id="projects">
+                           <h4 class="title">${item.name}</h4>
+                            <span class="data-create">${Intl.DateTimeFormat("pt-BR").format(new Date(item.created_at))}</span>
+                    </section>
+                    <section>
+                        <a href="${item.html_url}" target="_blank">${item.html_url}</a>
+                        <span class="language">
+                        <span class="circle"></span>
+                           ${item.language}
+                        </span>
+                    </section>`
+
+                repositories.appendChild(project);
+            })
+
+        })
+}
+
+getAPIGithubRepositories();
